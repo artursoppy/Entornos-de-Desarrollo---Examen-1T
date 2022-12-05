8bd7e42a473dd1b6ddf647c9b57bcd15ed9fe3bb
Artur SP <artur_soppy@proton.me>
2022-12-05 17:31:19 +0100

    aordenando en paquetes

--- src/com/empresa/tutorial_java/equals/CompareWithEquals.java
+++ src/com/empresa/tutorial_java/equals/CompareWithEquals.java
@@ -1,11 +1,6 @@
-class Persona {
-    private final String nombre;
-    public Persona(String nombre) {
-        this.nombre = nombre;
-    }
-}
+package com.empresa.tutorial_java.equals;
 
-public class CompareWithEquals {
+class CompareWithEquals {
 
     public static void main(String... argv) {
 
@@ -16,12 +11,10 @@ public class CompareWithEquals {
         System.out.println(p.equals(p1)); // false
         System.out.println(p == p2); // true
         System.out.println(p.equals(p2)); // true
-
-
         String s1 = "hola";
         String s2 = s1;
         String s3 = "hola";
-        String s4 =  new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
+        String s4 = new String("hola"); // aunque el IDE sugiere no hacer explicita la instancia,...
 
         System.out.println(s1 == s2); // true
         System.out.println(s1 == s3); // true
@@ -29,7 +22,5 @@ public class CompareWithEquals {
         System.out.println(s1.equals(s2)); // true
         System.out.println(s1.equals(s3));  // true
         System.out.println(s1.equals(s4)); // true: referencias distintas a objetos iguales
-
     }
-
 }
